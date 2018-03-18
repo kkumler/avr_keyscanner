1b3b1aee2d1de95b01871cb1dc584f32cca00721
Jesse Vincent <jesse@keyboard.io>
2018-03-18 01:53:11 -0700

    Temporarily disable the ability to set the keyscan interval

--- firmware/wire-protocol.c
+++ firmware/wire-protocol.c
@@ -33,7 +33,7 @@ void twi_data_received(uint8_t *buf, uint8_t bufsiz) {
     case TWI_CMD_KEYSCAN_INTERVAL:
         if (bufsiz == 2 ) {
             // SET the delay
-            OCR1A = buf[1];
+            //OCR1A = buf[1];
         } else {
             // GET configuration
             twi_command = TWI_CMD_KEYSCAN_INTERVAL;
