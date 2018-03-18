ec75f4a0669e788562a2978d3beaf0d2e2bf950f
Jesse Vincent <jesse@keyboard.io>
2018-03-18 02:02:25 -0700

    Switch to a new integrator-based debouncing algorithm

--- firmware/debounce.h
+++ firmware/debounce.h
@@ -2,6 +2,28 @@
 
 #include <stdint.h>
 
+
+#define EXPECT_FALSE 0
+#define EXPECT_TRUE 1
+
+
+// #define DEBOUNCE_MINIMUM_MS 5
+// #define KEYSCAN_INTERVAL_TO_MS_MULTIPLIER 0.032
+// #define KEYSCAN_TIME  (KEYSCAN_INTERVAL  * KEYSCAN_INTERVAL_TO_MS_MULTIPLIER)
+// #define DEBOUNCE_MINIMUM_CYCLES ((DEBOUNCE_MINIMUM_MS/KEYSCAN_TIME)+1)
+
+#define DEBOUNCE_COUNTER_LIMIT 6
+#define DEBOUNCE_THRESHOLD 3
+
+
+
+
+static int8_t debounce_integrator_ceiling = DEBOUNCE_COUNTER_LIMIT;
+static int8_t debounce_integrator_floor = (0-DEBOUNCE_COUNTER_LIMIT);
+static int8_t debounce_toggle_on_threshold = DEBOUNCE_THRESHOLD;
+static int8_t debounce_toggle_off_threshold =  (0-DEBOUNCE_THRESHOLD);
+
+
 /*
 each of these 8 bit variables are storing the state for 8 keys
 
@@ -9,8 +31,7 @@ so for key 0, the counter is represented by db0[0] and db1[0]
 and the state in state[0].
 */
 typedef struct {
-    uint8_t db0;    // counter bit 0
-    uint8_t db1;    // counter bit 1
+    int8_t counters[8];
     uint8_t state;  // debounced state
 } debounce_t;
 
@@ -42,25 +63,78 @@ typedef struct {
  * And a way to flip selected bits in a variable or register:
  *   Set B to 1, then A ^ B = !A
  */
-static inline uint8_t debounce(uint8_t sample, debounce_t *debouncer) {
-    uint8_t delta, changes;
+static uint8_t debounce(uint8_t sample, debounce_t *debouncer) {
+    uint8_t changes = 0;
 
-    // Use xor to detect changes from last stable state:
-    // if a key has changed, it's bit will be 1, otherwise 0
-    delta = sample ^ debouncer->state;
 
-    // Increment counters and reset any unchanged bits:
-    // increment bit 1 for all changed keys
-    debouncer->db1 = ((debouncer->db1) ^ (debouncer->db0)) & delta;
-    // increment bit 0 for all changed keys
-    debouncer->db0 = ~(debouncer->db0) & delta;
+    // Scan each pin from the bank
+    for(int8_t i=0; i<=7; i++) {
+        // If the pin is on
+        if (__builtin_expect(( sample & _BV(i)), EXPECT_FALSE) ) {  // It's probably not on
 
-    // Calculate returned change set: if delta is still true
-    // and the counter has wrapped back to 0, the key is changed.
 
-    changes = ~(~delta | (debouncer->db0) | (debouncer->db1));
-    // Update state: in this case use xor to flip any bit that is true in changes.
-    debouncer->state ^= changes;
+            // If the counter for this key is below the threshold
+            // In this case, the threshold is half the number of cycles we need to debounce the switch + however
+            // far we want to overscan before starting the journey back to "off"
+            //
+            // The overscan allows us to more easily 'lock out' key chatter events by ignoring
+            // state changes for longer after detecting an event on a specific key
+            //
+            // It'll usually not be filled
+            if (__builtin_expect(( debouncer->counters[i] < debounce_integrator_ceiling ), EXPECT_TRUE) ) {
+                //Increment the counter
+                debouncer->counters[i]++;
+
+                // If the counter is at exactly the positivethreshold, then it's
+                // time to consider toggling the key state.
+                // Note that we'll hit the threshold twice during the period when the
+                // switch should be on. Once on the way up and once on the way back down.
+
+                if (debouncer->counters[i] == debounce_toggle_on_threshold) {
+                    // If the debounced state is currently 'off'...
+                    // (It's more likely the case that by the time we hit this code path,
+                    //  the debounced state would be on. We'd only be toggling it
+                    //  the first time we got here after the counter filled)
+                    if (__builtin_expect( (debouncer->state & _BV(i)), EXPECT_FALSE) ) {
+                        // record the change to return to the caller
+                        changes |= _BV(i);
+                        // Toggle the debounced state.
+                        debouncer->state ^= _BV(i);
+                    }
+                }
+            }
+            // If the pin is off
+        } else {
+            // If the counter isn't bottomed out
+            // (It'll usually be bottomed out)
+            if (__builtin_expect(( debouncer->counters[i] > debounce_integrator_floor), EXPECT_FALSE) ) {
+
+                // Decrement the counter
+                debouncer->counters[i]--;
+
+                // If the counter is at exactly the negative threshold, then it's
+                // time to consider toggling the key state.
+                // Note that we'll hit the threshold twice during the period when the
+                // switch should be off. Once on the way down and once on the way back up.
+                if (debouncer->counters[i] == debounce_toggle_off_threshold) {
+
+                    // If the debounced state is currently 'on'...
+                    // (It's more likely the case that by the time we hit this code path,
+                    //  the debounced state would be offf. We'd only be toggling it
+                    //  the first time we got here after the counter emptied
+                    if (__builtin_expect(  (debouncer->state ^ _BV(i)), EXPECT_FALSE) ) {
+
+                        // record the change to return to the caller
+                        changes |= _BV(i);
+                        // Toggle the debounced state.
+                        debouncer->state ^= _BV(i);
+                    }
+                }
+
+            }
+        }
+
+    }
 
     return changes;
 }
